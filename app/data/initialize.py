1a061b3b68a73dd9fd481dfdc5c62f7a6a728d08
Aaron Luna <contact@aaronluna.dev>
2021-04-02 17:19:22 -0700

    minor refatorings to dada.initialize module

--- app/data/initialize.py
+++ app/data/initialize.py
@@ -3,23 +3,25 @@ from pathlib import Path
 from zipfile import ZipFile
 
 DATA_FOLDER = Path(__file__).parent
-
+DOTENV_FILE = DATA_FOLDER.joinpath(".env")
+CONFIG_FILE = DATA_FOLDER.joinpath("vig.config.json")
+DATABASE_URL = f"sqlite:///{DATA_FOLDER.joinpath('vig.db')}"
 ZIP_FILES = [
-    (DATA_FOLDER.joinpath("vig.db.zip"), DATA_FOLDER),
-    (DATA_FOLDER.joinpath("json/2017/2017.zip"), DATA_FOLDER.joinpath("json/2017")),
-    (DATA_FOLDER.joinpath("json/2018/2018.zip"), DATA_FOLDER.joinpath("json/2018")),
-    (DATA_FOLDER.joinpath("json/2019/2019.zip"), DATA_FOLDER.joinpath("json/2019")),
+    DATA_FOLDER.joinpath("vig.db.zip"),
+    DATA_FOLDER.joinpath("json/2017/2017.zip"),
+    DATA_FOLDER.joinpath("json/2018/2018.zip"),
+    DATA_FOLDER.joinpath("json/2019/2019.zip"),
 ]
 
 
 def extract_zip_files():
-    for (zip_file, extract_path) in ZIP_FILES:
+    for zip_file in ZIP_FILES:
         with ZipFile(zip_file, mode="r") as zip:
-            zip.extractall(path=extract_path)
+            zip.extractall(path=zip_file.parent)
         zip_file.unlink()
 
 
 def set_env_variables():
-    os.environ["DOTENV_FILE"] = str(DATA_FOLDER.joinpath(".env"))
-    os.environ["CONFIG_FILE"] = str(DATA_FOLDER.joinpath("vig.config.json"))
-    os.environ["DATABASE_URL"] = f"sqlite:///{DATA_FOLDER.joinpath('vig.db')}"
+    os.environ["DOTENV_FILE"] = str(DOTENV_FILE)
+    os.environ["CONFIG_FILE"] = str(CONFIG_FILE)
+    os.environ["DATABASE_URL"] = DATABASE_URL
